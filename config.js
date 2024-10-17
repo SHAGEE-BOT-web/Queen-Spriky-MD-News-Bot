a7e66e2ddf517e244b858bc025be8c94be3a90d8
SHAGE-MD V3 <PRABATHbot@outlook.com>
2024-10-17 10:01:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'gitVURgJ#NG2oOiuAw9h9WQtSxldOglb-LynI_AmPoOr58h-jZGE', // Enter Your Session ID
     MONGODB: process.env.MONGODB || '',    // Enter Your MongoDB URL
     Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
     BotNumber: process.env.BotNumber || '94773366833'    // Enter Your Bot Number
